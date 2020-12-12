618728e67e975bf6ea89857172e04482ecb1cc70
yodamk <yodamk1234@gmail.com>
2020-12-12 15:20:33 +0900

    Changes to be committed: 	modified:   .DS_Store 	new file:   templates/base.html 	new file:   templates/login.html 	new file:   templates/signup.html 	modified:   todo/admin.py 	new file:   todo/migrations/0001_initial.py 	modified:   todo/models.py 	modified:   todo/urls.py 	modified:   todo/views.py

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
+
+
 from django.http import HttpResponse
 
-# Create your views here.
+
 def todo(request):
     return HttpResponse('Hello!')
+
+
+def signupview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています'})
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {})
+
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = authenticate(request, username=username_data,
+                            password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
